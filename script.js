b96615cfd1ff2ffdfa19b7f5a04782c3363a8bae
Mukaddim Rahi <mukadddimrahi@gmail.com>
2023-10-07 22:46:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// // JavaScript for handling login
+// const loginButton = document.getElementById('login-button');
+// const emailInput = document.getElementById('email');
+// const passwordInput = document.getElementById('password');
+
+// loginButton.addEventListener('click', () => {
+//     const email = emailInput.value;
+//     const password = passwordInput.value;
+
+//     // You can add your login logic here
+//     // For this example, we'll just show an alert
+//     if (email && password) {
+//         alert(`Logged in with email: ${email}`);
+//     } else {
+//         alert('Please enter both email and password.');
+//     }
+// });
